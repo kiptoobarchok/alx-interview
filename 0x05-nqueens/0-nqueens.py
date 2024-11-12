f653b16c52eb5104e34173381f2d92cbd607cb69
kiptoobarchok <calebkiptoo9090@gmail.com>
2024-11-12 14:13:09 +0000

    alx-interview

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,64 +1,66 @@
 #!/usr/bin/python3
 """
-A program that solves the N queens problem.
+    N-queen problem
+    The next algo solve any N queen in any NxN
+    Being N > 3
 """
-
 import sys
 
 
-def print_usage_and_exit():
-    print("Usage: nqueens N")
-    sys.exit(1)
+def n_q(t_arr, arr, col, i, n):
+    """
+       n_q - Find all posibles solution for N-queen problem and return it
+             in a list
+       @t_arr: temporaly list to store the all points of a posible solution
+       @arr: store all the solution
+       @col: save a colum use for a queen
+       @i: the row of the chess table
+       @n: Number of queens
+    """
+    if (i > n):
+        arr.append(t_arr[:])
+        return arr
 
+    for j in range(n + 1):
+        if i == 0 or ([i - 1, j - 1] not in t_arr and
+                      [i - 1, j + 1] not in t_arr and
+                      j not in col):
+            if i > 1:
+                dia = 0
+                for k in range(2, i + 1):
+                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
+                        dia = 1
+                        break
+                if dia:
+                    continue
+            t_arr.append([i, j])
+            col.append(j)
+            n_q(t_arr, arr, col, i + 1, n)
+            col.pop()
+            t_arr.pop()
 
-def print_error_and_exit(message):
-    print(message)
-    sys.exit(1)
-
-
-def is_valid(board, row, col):
-    """Check if a queen can be placed on board at row, col"""
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
-
-
-def solve_nqueens(N):
-    def solve(row):
-        if row == N:
-            solutions.append([[i, board[i]] for i in range(N)])
-            return
-        for col in range(N):
-            if is_valid(board, row, col):
-                board[row] = col
-                solve(row + 1)
-                board[row] = -1
-
-    board = [-1] * N
-    solutions = []
-    solve(0)
-    return solutions
-
-
-def main():
-    if len(sys.argv) != 2:
-        print_usage_and_exit()
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print_error_and_exit("N must be a number")
-
-    if N < 4:
-        print_error_and_exit("N must be at least 4")
-
-    solutions = solve_nqueens(N)
-    for solution in solutions:
-        print(solution)
+    return arr
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except BaseException:
+        print("N must be a number")
+        exit(1)
+
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
+
+    elif n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    n_q_arr = n_q([], [], [], 0, n - 1)
+    for i in n_q_arr:
+        print(i)
