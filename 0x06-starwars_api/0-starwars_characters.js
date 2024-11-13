52a02464f3f04082baf89f99db0f9a321ac5b391
kiptoobarchok <calebkiptoo9090@gmail.com>
2024-11-13 16:44:04 +0000

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,58 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Get the Movie ID from the command line arguments
 const movieId = process.argv[2];
-
-// Check if movieId is provided
-if (!movieId) {
-  console.error('Please provide a Movie ID');
-  process.exit(1);
-}
-
-// Define the URL for the Star Wars API
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-// Function to fetch character name
-const fetchCharacterName = (characterUrl) => {
-  return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const character = JSON.parse(body);
-        resolve(character.name);
-      }
-    });
-  });
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
 };
 
-// Make a request to the Star Wars API
-request(url, async (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  // Parse the response body as JSON
-  const film = JSON.parse(body);
-
-  // Check if film is found
-  if (!film.title) {
-    console.error('Movie not found');
-    return;
-  }
-
-  // Get the list of character URLs
-  const characters = film.characters;
-
-  // Fetch all character names
-  try {
-    const characterNames = await Promise.all(characters.map(fetchCharacterName));
-    // Print each character name in the correct order
-    characterNames.forEach(name => console.log(name));
-  } catch (fetchError) {
-    console.error('Error:', fetchError);
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
